e94d66a76fdf1b372421a7a7d95c94b9086e9447
MohsinAliJafery <70712589+MohsinAliJafery@users.noreply.github.com>
2025-04-03 20:11:24 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  API_URL: 'https://pekerdja.co/api/',
+  // API_URL: 'https://pekerdja.co/api/',
+  API_URL: 'http://82.25.115.62:5000/api/',
   CUR_URL: 'https://v6.exchangerate-api.com/v6/27628141ceaf7330818698cc/pair/IDR/USD',
 };
